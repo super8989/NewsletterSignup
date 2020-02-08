7b3a9ceebd94f05a371125b5a5997a0ad3b9e21a
Sam <super8989@gmail.com>
2020-02-08 16:46:14 -0500

    post data to mailchimp server

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const request = require('request');
 const app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
+
+require('dotenv').config();
+const API = process.env.API;
+console.log(process.env);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
 
@@ -18,5 +26,38 @@ app.post('/', (req, res) => {
 	const lastName = req.body.lName;
 	const email = req.body.email;
 
-	console.log(req.body);
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: 'subscribed',
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName
+				}
+			}
+		]
+	};
+
+	const jsonData = JSON.stringify(data);
+
+	const options = {
+		url: 'https://us4.api.mailchimp.com/3.0/lists/795a2a1b64',
+		method: 'POST',
+		headers: {
+			Authorization: `sam ${API}`
+		},
+		body: jsonData
+	};
+
+	request(options, (error, response, body) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(response.statusCode);
+		}
+	});
 });
+
+// List id
+// 795a2a1b64

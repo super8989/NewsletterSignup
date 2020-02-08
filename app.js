3fd3052ea7f42baacfe1b2fdbcefabf705465e87
Sam <super8989@gmail.com>
2020-02-08 14:47:35 -0500

    serving static files in express

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.listen(3000, () => console.log('Server is running on port 3000'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/signup.html');
+});

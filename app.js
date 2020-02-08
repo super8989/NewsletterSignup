c0d1f0f1669f7f6f74f0426951d6be8b452f0b9e
Sam <super8989@gmail.com>
2020-02-08 15:04:27 -0500

    set up post request

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const request = require('request');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+	const firstName = req.body.fName;
+	const lastName = req.body.lName;
+	const email = req.body.email;
+
+	console.log(req.body);
 });
